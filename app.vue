705093e2f8e3b7cd4cbc750ccc1a13ae4d415eab
avery.howard.1 <avery.howard.1@us.af.mil>
2023-08-02 20:55:16 -0400

    Initial upload

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-20 h-20 bg-blue-600">
+    Hello
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,vue}',
+    './pages/**/*.{js,ts,jsx,tsx,vue}',
+    './app.vue'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
