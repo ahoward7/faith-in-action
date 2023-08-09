19dcafb6cc991e694ec09fc8f57b038fae7e7e78
avery.howard.1 <avery.howard.1@us.af.mil>
2023-08-09 09:42:58 -0400

    Images and responsive design

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <div class="flex justify-evenly items-center flex-col gap-20 py-20 2xl:flex-row 2xl:gap-0 bg-fia-white">
-      <img src="~/assets/images/logos/fia-logo-transparent.png" style="height: 600px;">
-      <img src="~/assets/images/logos/fia-text-transparent.png" class="h-80">
+  <div class="flex flex-col items-center">
+    <img src="~/assets/images/headers/faith-in-sand-short.jpg" class="w-full border-b-4 border-fia-black 2xl:border-b-8">
+    <div class="max-w-1920 w-full flex flex-col">
+      <div class="flex justify-around items-center flex-col gap-20 py-20  2xl:flex-row 2xl:gap-0 ">
+        <img src="~/assets/images/logos/fia-logo-transparent.png" class="h-400 xl:h-500 3xl:h-600">
+        <img src="~/assets/images/logos/fia-text-transparent.png" class="h-60 xl:h-72 3xl:h-80">
+      </div>
     </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
         'fia-white': '#f8f8f8',
         'fia-black': '#1d1d1d',
         'fia-blue': '#0041EF',
+      },
+      height: {
+        '400': '400px',
+        '500': '500px',
+        '600': '600px',
+      },
+      maxWidth: {
+        '1920': '1920px',
+      },
+      screens: {
+        '3xl': '1920px',
       }
     },
   },
