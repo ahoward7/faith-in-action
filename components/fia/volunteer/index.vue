2d13deb81e93d228d1d8a9439571e5853e5f204b
avery.howard.1 <avery.howard.1@us.af.mil>
2023-08-10 14:11:45 -0400

    Small style change

--- components/fia/volunteer/index.vue
+++ components/fia/volunteer/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full flex justify-center bg-fia-yellow">
     <div class="max-w-1920 w-full grid grid-cols-1 2xl:grid-cols-2 pt-10 lg:pt-20 2xl:py-40">
-      <fia-volunteer-section header="Learn about different ways you can serve the older generation." button="Sign Up" anchor="/volunteer" class="pb-20 px-20">
+      <fia-volunteer-section header="Learn about different ways you can serve the older generation." button="Sign Up" anchor="/volunteer" class="px-20 pb-20 pt-10 2xl:pb-20 2xl:pt-0">
         <img src="~/assets/images/logos/fia-volunteer-text.png" class="max-h-24 lg:max-h-24 2xl:max-height-28">
       </fia-volunteer-section>
       <fia-volunteer-section header="Help those in need by supporting us with a donation." button="Support" anchor="/" boxStyle="bg-fia-yellow 2xl:bg-fia-white" class="px-20 py-20 2xl:pb-20 2xl:pt-0 bg-fia-white 2xl:bg-fia-yellow">
