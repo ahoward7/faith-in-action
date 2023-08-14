7dd2c4d9aaeeb8408290af96be7b65ad69e15404
avery.howard.1 <avery.howard.1@us.af.mil>
2023-08-14 18:33:13 -0400

    Services text written

---
+++ components/fia/services/Section.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full flex gap-20">
+    <div class="basis-1/2 flex flex-col items-center justify-center">
+      <div class="w-full text-5xl text-fia-yellow font-bold mb-4">
+        <slot name="left-header" />
+      </div>
+      <slot name="left"/>
+    </div>
+    <div class="basis-1/2 flex flex-col items-center justify-center">
+      <div class="w-full text-5xl text-fia-yellow font-bold mb-4">
+        <slot name="right-header" />
+      </div>
+      <slot name="right"/>
+    </div>
+  </div>
+</template>
