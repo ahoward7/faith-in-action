d7dfec0458c31bb665ec2c5dcba6e470acc52262
avery.howard.1 <avery.howard.1@us.af.mil>
2023-08-16 10:12:40 -0400

    Donate button

--- components/fia/button/Donate.vue
+++ components/fia/button/Donate.vue
@@ -1,3 +1,3 @@
 <template>
-  <fia-navbar-item to="/" class="bg-fia-blue px-3 py-2 justify-center">Donate</fia-navbar-item>
+  <fia-navbar-item to="https://www.paypal.com/donate/?hosted_button_id=PLDCK2H2BJ7U2" target="_blank" class="bg-fia-blue px-3 py-2 justify-center">Donate</fia-navbar-item>
 </template>
