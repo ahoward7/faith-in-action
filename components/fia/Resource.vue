053d40cdb088e0e19f7b57a00760e05371a536ab
Avery Howard <avery.d.howard@gmail.com>
2025-04-03 20:44:23 -0400

    Resource hover effect

---
+++ components/fia/Resource.vue
@@ -0,0 +1,26 @@
+<template>
+  <NuxtLink target="_blank" :to="url" class="flex justify-center items-center object-contain cursor-pointer">
+    <div class="thing hover:scale-105 duration-300" :class="imgClass">
+      <slot class="" />
+    </div>
+  </NuxtLink to="">
+</template>
+
+<script setup lang="ts">
+defineProps({
+  url: {
+    type: String,
+    default: 'https://nvcnetwork.org/'
+  },
+  imgClass: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<style scoped>
+.thing:hover {
+  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.15);
+}
+</style>
